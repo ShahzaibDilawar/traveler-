124cb1f54375a304977737a652eb62c4c1a32046
Shahzaib Dilawar <shahzaibdilawar@gmail.com>
2017-08-25 07:37:00 -0700

    footer file is added

---
+++ inc/footer.php
@@ -0,0 +1,36 @@
+<style>
+	/* Sticky footer styles
+-------------------------------------------------- */
+html {
+  position: relative;
+  min-height: 100%;
+}
+body {
+  /* Margin bottom by footer height */
+  margin-bottom: 60px;
+}
+.footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  /* Set the fixed height of the footer here */
+  height: 60px;
+  background-color: #f5f5f5;
+}
+
+
+</style>
+<footer class="footer">
+      <div class="container">
+        <p class="text-center">	
+        Choose Language: 
+        <center>
+        <div id="google_translate_element"></div><script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
+</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+</center>
+        </p>
+      </div>
+    </footer>
